cd59d0ed1b857ab64e667f09f7845b7bd85cf795
Dan Dyar <dan@parameterz.com>
2023-08-13 16:30:05 -0700

    Update script.js
    more troubleshooting; had two scripts...

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 const form = document.getElementById('volunteerForm');
 const messageDiv = document.getElementById('message');
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-
-    // You can add validation here if needed
-
-    const message = `Thank you, ${name}, for volunteering! We'll contact you at ${email}.`;
-
-    messageDiv.textContent = message;
-    form.reset();
-});
-
 
 form.addEventListener('submit', async function (event) {
     event.preventDefault();
