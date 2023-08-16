4c1d3970984b03eeb2572f9e520cd9ca9444c92c
Dan Dyar <dan@parameterz.com>
2023-08-15 19:15:45 -0700

    Update script.js
    need to add some JQuery magic sauce to make this work

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ form.addEventListener('submit', async function (event) {
 const volunteerCountElement = document.getElementById('volunteerCount');
 
 async function updateVolunteerCount() {
+    //replace with JQuery AJAX
     const url = 'https://script.google.com/macros/s/AKfycbwoTfFf7cq4gkjZfKgBlgG5GnlUMe2grZLD_Ka_yAfZVETyDg5SjHslrOAE5cExZxr5aQ/exec'; // The URL of your deployed volunteer count web app
 
     const response = await fetch(url, {
@@ -48,3 +49,27 @@ async function updateVolunteerCount() {
 
 // Call the function when the page loads
 updateVolunteerCount();
+
+/* NOTE: 
+This example works in theory, but the script runs on the hit to the script address, but redirects (302) to another address to show the result.
+This behavior is not handled well with this code (throws this error every time: 'Error fetching volunteer count.')
+HOWEVER, I ran this as an AJAX call using JQuery and it works fine!
+
+https://www.tutorialsteacher.com/codeeditor?cid=jquery-101
+
+	<script type="text/javascript">
+        $(document).ready(function () {
+			
+		 $('#ajaxBtn').click(function(){
+			$.ajax('https://script.google.com/macros/s/AKfycbwoTfFf7cq4gkjZfKgBlgG5GnlUMe2grZLD_Ka_yAfZVETyDg5SjHslrOAE5cExZxr5aQ/exec',   // request url
+			{            
+				success: function (data, status, xhr) {    // success callback function
+						$('p').append(data);
+				}
+			});
+		 });
+
+        });
+    </script>
+
+*/
