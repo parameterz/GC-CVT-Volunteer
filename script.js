a12c2e82b7dd3498cd1d324d4606bebe06b80b39
Dan Dyar <dan@parameterz.com>
2023-08-17 21:02:00 -0700

    Update script.js
    converting to Jquery

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+// from ChatGPT
+const volunteerCountElement = $('#volunteerCount');
+
+async function updateVolunteerCount() {
+    const url = 'https://script.google.com/macros/s/AKfycbwoTfFf7cq4gkjZfKgBlgG5GnlUMe2grZLD_Ka_yAfZVETyDg5SjHslrOAE5cExZxr5aQ/exec'; // The URL of your deployed volunteer count web app
+
+    try {
+        const count = await $.get(url);
+        volunteerCountElement.text(count);
+    } catch (error) {
+        volunteerCountElement.text('Error fetching volunteer count.');
+    }
+}
+
+$(document).ready(function () {
+    $('#volunteerForm').submit(async function (event) {
+        event.preventDefault();
+
+        const name = $('#name').val();
+        const email = $('#email').val();
+
+        const url = 'https://script.google.com/macros/s/AKfycbzXmYCuNi5zXs2R3WHBbfTF8zvzjI30PKsnyRQFRmofqJYVy1lL0ByEcA-3_Tqz1ejvuQ/exec'; // The URL of your deployed web app
+
+        try {
+            const response = await $.post(url, {
+                name: name,
+                email: email
+            });
+
+            if (response === "Volunteer added successfully.") {
+                $('#message').text(response);
+                $('#volunteerForm')[0].reset();
+                updateVolunteerCount(); // Update the count after successful submission
+            } else {
+                $('#message').text('Error submitting the form.');
+            }
+        } catch (error) {
+            $('#message').text('Error submitting the form.');
+        }
+    });
+
+    updateVolunteerCount(); // Update the count when the page loads
+});
+
+
+
+/* 
+
 const form = document.getElementById('volunteerForm');
 const messageDiv = document.getElementById('message');
 
@@ -69,3 +117,4 @@ https://www.tutorialsteacher.com/codeeditor?cid=jquery-101
     </script>
 
 */
+
