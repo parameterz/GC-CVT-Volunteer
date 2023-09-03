477dbf4527b79d8ae9b13db50213ce707acbf6d0
Dan Dyar <dan@parameterz.com>
2023-09-03 08:34:54 -0700

    adding logic for how many volunteers are still needed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // from ChatGPT
 const volunteerCountElement = $('#volunteerCount');
+const volunteersDesired = 8;
 
 async function updateVolunteerCount() {
     const url = 'https://script.google.com/macros/s/AKfycbwoTfFf7cq4gkjZfKgBlgG5GnlUMe2grZLD_Ka_yAfZVETyDg5SjHslrOAE5cExZxr5aQ/exec'; // The URL of your deployed volunteer count web app
@@ -7,6 +8,7 @@ async function updateVolunteerCount() {
     try {
         const count = await $.get(url);
         volunteerCountElement.text(count);
+        $("#needed").html(volunteersDesired-count);
     } catch (error) {
         volunteerCountElement.text('Error fetching volunteer count.');
     }
@@ -43,78 +45,4 @@ $(document).ready(function () {
 });
 
 
-
-/* 
-
-const form = document.getElementById('volunteerForm');
-const messageDiv = document.getElementById('message');
-
-
-form.addEventListener('submit', async function (event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-
-    const url = 'https://script.google.com/macros/s/AKfycbzXmYCuNi5zXs2R3WHBbfTF8zvzjI30PKsnyRQFRmofqJYVy1lL0ByEcA-3_Tqz1ejvuQ/exec'; // The URL of your deployed web app
-
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`
-    });
-
-    if (response.ok) {
-        const message = await response.text();
-        messageDiv.textContent = message;
-        form.reset();
-    } else {
-        messageDiv.textContent = 'Error submitting the form.';
-    }
-});
-
-
-
-    $(document).ready(function () {
-        const url = 'https://script.google.com/macros/s/AKfycbwoTfFf7cq4gkjZfKgBlgG5GnlUMe2grZLD_Ka_yAfZVETyDg5SjHslrOAE5cExZxr5aQ/exec'; // The URL of your deployed volunteer count web app
-
-        $.ajax(url,
-            {
-                success: function (data, status, xhr) {    // success callback function
-                    $('#volunteerCount').text(data);
-            }
-            });
-
-    });
-
-
-
-// Call the function when the page loads
-updateVolunteerCount();
-
-/* NOTE: 
-This example works in theory, but the script runs on the hit to the script address, but redirects (302) to another address to show the result.
-This behavior is not handled well with this code (throws this error every time: 'Error fetching volunteer count.')
-HOWEVER, I ran this as an AJAX call using JQuery and it works fine!
-
-https://www.tutorialsteacher.com/codeeditor?cid=jquery-101
-
-	<script type="text/javascript">
-        $(document).ready(function () {
-			
-		 $('#ajaxBtn').click(function(){
-			$.ajax('https://script.google.com/macros/s/AKfycbwoTfFf7cq4gkjZfKgBlgG5GnlUMe2grZLD_Ka_yAfZVETyDg5SjHslrOAE5cExZxr5aQ/exec',   // request url
-			{            
-				success: function (data, status, xhr) {    // success callback function
-						$('p').append(data);
-				}
-			});
-		 });
-
-        });
-    </script>
-
-*/
 
