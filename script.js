4769fb68ccd394cafd6f3f9b72d1e64b9b3d68e4
Dan Dyar <dan@parameterz.com>
2023-09-03 20:59:41 -0700

    Update script.js
    trying to add date to the form post

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         const email = $('#email').val();
         const date = Date();
 
-        const url = 'https://script.google.com/macros/s/AKfycbzXmYCuNi5zXs2R3WHBbfTF8zvzjI30PKsnyRQFRmofqJYVy1lL0ByEcA-3_Tqz1ejvuQ/exec'; // The URL of your deployed web app
+        const url = 'https://script.google.com/macros/s/AKfycbzu2_hGo2dpzYAOu_aSf5mlnN8mqg2heCjMWskCs5_ZJpQSjj5KJk2FhR7IzZ4jDA5puQ/exec'; // The URL of your deployed web app
 
         try {
             const response = await $.post(url, {
